a0d8559a1ee352c663550b5d7924e0469f55aab4
brol.sergii <brol.sergii@gmail.com>
2017-04-15 13:13:22 +0200

    Adds auxilary methods

--- codersOfTheCaribbean/Program.cs
+++ codersOfTheCaribbean/Program.cs
@@ -5,8 +5,62 @@ using System.Text;
 using System.Collections;
 using System.Collections.Generic;
 
+class Ship
+{
+    public Ship(int orientation, int speed, int rhum, int player, int x, int y)
+    {
+        Orientation = orientation;
+        Speed = speed;
+        Rhum = rhum;
+        Player = player;
+        X = x;
+        Y = y;
+    }
+
+    public int Orientation;
+    public int Speed;
+    public int Rhum;
+    public int Player;
+    public int X;
+    public int Y;
+}
+
+class Barrel
+{
+    public Barrel(int capasity, int x, int y)
+    {
+        Capasity = capasity;
+        X = x;
+        Y = y;
+    }
+
+    public int Capasity;
+    public int X;
+    public int Y;
+}
+
 class Player
 {
+
+    #region Auxilary methods
+    static void TryCatch(Action a) { try { a(); } catch (Exception) { } }
+    static int TryGetInt(Func<int> a) { try { return a(); } catch (Exception) { return 0; } }
+    static void Deb(object o) => Console.Error.WriteLine(o);
+    static void DebList(IEnumerable<object> e) => Console.Error.WriteLine(e.Aggregate((x, y) => $"{x} {y}"));
+    static void DebObjList(IEnumerable<object> e) => TryCatch(() => Console.Error.WriteLine(e.Aggregate((x, y) => $"{x}\n{y}")));
+    static void DebDict(Dictionary<int, int> d)
+    {
+        foreach (var pair in d)
+            Console.Error.WriteLine($"[{pair.Key}]:{pair.Value}");
+    }
+    #endregion
+
+    #region Static global game state
+    static int[,] MAP;
+    static List<Ship> ships = new List<Ship>();
+    static List<Barrel> barrels = new List<Barrel>();
+    #endregion
+
     static void Main(string[] args)
     {
         while (true)
